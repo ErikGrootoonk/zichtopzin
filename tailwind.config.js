a158f4f773801d9252f19a0f59d8d6f8e046b862
erik <hgrootoonk@proton.me>
2024-05-11 14:20:35 +0200

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export const content = ["./*.{html,js}", "./src/*.js"];
+export const theme = {
+  extend: {
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+    },
+  },
+  plugins: [],
+};
+
