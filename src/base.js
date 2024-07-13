aad7d991e8281cbcd92eec13a0182d57ae53f0fa
erik <erik.grootoonk@gmail.com>
2024-07-13 16:13:08 +0200

    js aanpassing

--- src/base.js
+++ src/base.js
@@ -2,7 +2,7 @@ const navBar = document.querySelector("#navbar");
     const threshold = navBar.offsetHeight;
 
     window.addEventListener("scroll", function() {
-      const scrollPosition = window.pageYOffset || window.scrollY;
+      const scrollPosition = window.scrollY;
 
       if (scrollPosition >= threshold) {
         navBar.classList.add("fixed", "top-0", "left-0", "right-0", "z-50");
